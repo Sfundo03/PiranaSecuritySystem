c5c11e7bbb212fb990810dd978ac7a7f16c1e8f2
zanoxolo35 <mqwelzanoxolo09@gmail.com>
2025-09-18 19:10:51 +0200

    abc

---
+++ Migrations/202509152327025_newGroup.cs
@@ -0,0 +1,18 @@
+﻿namespace PiranaSecuritySystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newGroup : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Guards", "Group", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Guards", "Group");
+        }
+    }
+}
---
+++ Migrations/202509152332019_newStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace PiranaSecuritySystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newStatus : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Guards", "Status", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Guards", "Status");
+        }
+    }
+}
